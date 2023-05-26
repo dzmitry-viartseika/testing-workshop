f3566cb6e978ea67b3f46c7a2d96eea57cd36251
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-05-26 23:36:36 +0200

    implement selectRandomQuote unit test

---
+++ src/hooks/useConverter.test.js
@@ -0,0 +1,8 @@
+describe('when rendered', function () {
+    it.todo('RUB input should have a value equal to initial')
+    it.todo('USD input should have a value equal to calcaulated USD amount')
+});
+
+describe('when called an `setUsdValue` method', function () {
+    it.todo('')
+});
