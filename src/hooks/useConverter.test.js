c8e37f679ccc8f6e50ec580f145d710f14980c0e
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-05-27 10:14:23 +0200

    implement useConverter

--- src/hooks/useConverter.test.js
+++ src/hooks/useConverter.test.js
@@ -1,8 +1,18 @@
-describe('when rendered', function () {
+describe('when rendered', () => {
     it.todo('RUB input should have a value equal to initial')
     it.todo('USD input should have a value equal to calcaulated USD amount')
 });
 
-describe('when called an `setUsdValue` method', function () {
-    it.todo('')
+describe('when called an `setRubValue` method', () => {
+    it.todo('should update the RUB value')
+    it.todo('should recalculate the USD value')
 });
+
+describe('when called an `setUsd Value` method', () => {
+    it.todo('should update the USD value')
+    it.todo('should recalculate the RUB value')
+})
+
+describe('when re-rendered', () => {
+    it.todo('should update its value')
+})
