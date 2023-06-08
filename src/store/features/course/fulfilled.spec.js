ed3d31faa040c4cc335a48651297434d2744f8c5
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-08 20:34:37 +0200

    implement fulfilled and rejected unit tests

--- src/store/features/course/fulfilled.spec.js
+++ src/store/features/course/fulfilled.spec.js
@@ -1,22 +1,18 @@
 import {initialCourseState} from "./slice";
 import {fetchTodayCourseFulfilled} from "./fulfilled";
-
-const LOADING_STATE = {
-    ...initialCourseState,
-    status: 'loading'
-}
+import {loadingCourseState} from "../../../testUtils/store";
 
 const IDLE = 'idle';
 
 const TEST_ACTION = {payload: 42}
 describe('when called with payload', () => {
     it('should return a state with the updated course', () => {
-        const result = fetchTodayCourseFulfilled(LOADING_STATE, TEST_ACTION);
+        const result = fetchTodayCourseFulfilled(loadingCourseState, TEST_ACTION);
         expect(result.value).toEqual(TEST_ACTION.payload)
     });
 
     it('should return a state with the loading status', () => {
-        const result = fetchTodayCourseFulfilled(LOADING_STATE, TEST_ACTION);
+        const result = fetchTodayCourseFulfilled(loadingCourseState, TEST_ACTION);
         expect(result.status).toEqual(IDLE);
     });
 })
