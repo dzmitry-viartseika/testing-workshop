c6f671ca9c4c23adb7d41affff5bc592960077b4
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-08 18:05:22 +0200

    add components CurrentCourse

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import { CurrentCourse } from "../CurrentCourse/CurrentCourse";
+import { RefreshCourse } from "../RefreshCourse";
+import styles from "./Header.module.css";
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            Курс доллара на сегодня:
+            <CurrentCourse />
+            <RefreshCourse />
+        </header>
+    );
+}
