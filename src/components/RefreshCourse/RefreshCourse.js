e8da1ee082e2deb878389a4bd85165540a308b26
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-08 19:26:18 +0200

    implement unit tests

---
+++ src/components/RefreshCourse/RefreshCourse.js
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+    selectStatus,
+    selectError,
+    fetchTodayCourse,
+} from "../../store/features/course/slice";
+
+export function RefreshCourse() {
+    const status = useSelector(selectStatus);
+    const error = useSelector(selectError);
+    const isLoading = status === "loading";
+
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <button type="button" onClick={() => dispatch(fetchTodayCourse())}>
+                {isLoading ? "Обновляем курс..." : "Обновить курс"}
+            </button>
+            {error && <p>{error}</p>}
+        </>
+    );
+}
---
+++ src/components/RefreshCourse/RefreshCourse.spec.js
@@ -0,0 +1,29 @@
+import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/react";
+import { renderWithStore } from "../../testUtils";
+import { loadingCourseState, errorCourseState } from "../../testUtils/store";
+import { RefreshCourse } from "./RefreshCourse";
+
+describe("when the button is in initial state", () => {
+    it("should contain the default text", () => {
+
+    });
+});
+
+describe("when the button is in the loading state", () => {
+    it("should contain the corresponding text", () => {
+
+    });
+});
+
+describe("when the button is in an error state", () => {
+    it("should contain the error message", () => {
+
+    });
+});
+
+describe("when the button is clicked", () => {
+    it("should call the required action", () => {
+
+    });
+});
